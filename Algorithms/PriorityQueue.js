544003aeb5b9539136395e94c9fc78ab9a5e9a08
Abhishek1152 <abhisheksingh5275@gmail.com>
2020-04-13 11:25:23 +0530

    Added priority queue class
    Designed priority queue class so as to implement in Best First Search

---
+++ Algorithms/PriorityQueue.js
@@ -0,0 +1,71 @@
+// Designing priority queue
+// Reference from Geeksforgeeks
+
+class QElement 
+{
+    constructor(element, priority) 
+    { 
+        this.element = element; 
+        this.priority = priority; 
+    } 
+} 
+  
+class PriorityQueue 
+{
+    constructor() 
+    {
+        this.items = []; 
+    }
+
+    enqueue(element, priority) 
+	{ 
+	    // creating object from queue element 
+	    var qElement = new QElement(element, priority); 
+	    var contain = false; 
+	  
+	    // iterating through the entire 
+	    // item array to add element at the 
+	    // correct location of the Queue 
+	    for (var i = 0; i < this.items.length; i++) { 
+	        if (this.items[i].priority > qElement.priority) { 
+	            // Once the correct location is found it is 
+	            // enqueued 
+	            this.items.splice(i, 0, qElement); 
+	            contain = true; 
+	            break; 
+	        } 
+	    } 
+	  
+	    // if the element have the highest priority 
+	    // it is added at the end of the queue 
+	    if (!contain) { 
+	        this.items.push(qElement); 
+	    } 
+	}
+
+	dequeue() 
+	{
+	    // return the dequeued element 
+	    // and remove it. 
+	    // if the queue is empty 
+	    // returns Underflow 
+	    if (this.isEmpty()) 
+	        return "Underflow"; 
+	    return this.items.shift(); 
+	}
+
+	front() 
+	{
+	    // returns the highest priority element 
+	    // in the Priority queue without removing it. 
+	    if (this.isEmpty()) 
+	        return "No elements in Queue"; 
+	    return this.items[0]; 
+	}
+	
+	isEmpty() 
+	{
+	    // return true if the queue is empty. 
+	    return this.items.length == 0; 
+	}
+} 
